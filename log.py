1fb33d6b16b0ac6db9ddf16710d2713c7d7c95b8
griffing52 <emailgriffinnow@gmail.com>
2020-11-21 16:48:31 -0800

    Added delete and subWrite Functions to log

--- log.py
+++ log.py
@@ -1,7 +1,7 @@
 import json
 
 #parameters are the value you want to log and the name is a term used to refer to the value
-def writeLog(name, value, logFile="log.json"):
+def write(name, value, logFile="log.json"):
     writeData = {}
     writeData[name] = value
     with open(logFile, 'r+') as f:
@@ -10,24 +10,26 @@ def writeLog(name, value, logFile="log.json"):
         f.seek(0)
         json.dump(data, f)
     print('%s, %s' % (name, value))
-    
-def readLog(name, logFile="log.json"):
+
+#writes to a value in an object inside of the log file (e.g. "test": {"num": 12, "string": "Hello World"}  log.subWrite("test", "num", 41))
+def subWrite(name, subName, value, logFile="log.json"):
+    val = read(name, logFile)
+    val[subName] = value
+    write(name, val, logFile)
+
+def read(name, logFile="log.json"):
     with open(logFile, 'r') as f:
-        return json.load(f)[name]
+        try:
+            return json.load(f)[name]
+        except Exception as e: 
+            # print(e)
+            print("Value not found")
         
-def deleteLog(name, logFile="log.json"):
-    with open(logFile, 'r+') as f:
-        data =  json.load(f)
-        there = False
-        print(data)
-        for x in data:
-            if name in x:
-                there = True
-        if there:
-            del data[x]
-        #data.pop(name, None)
-        data = json.dump(data, f)
-        
-print(readLog('math'))
-deleteLog('math')
-print(readLog('math'))
+def delete(name, logFile="log.json"):
+    with open(logFile, 'r') as data_file:
+        data = json.load(data_file)
+
+    data.pop(name, None)
+
+    with open(logFile, 'w') as data_file:
+        data = json.dump(data, data_file)
